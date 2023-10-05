b270feddda7b479788b8c67f640f7a83a00593d5
KodaiCoder <nutdeknon@gmail.com>
2023-10-05 10:46:47 +0700

    try adding svelte-preprocess to handle post css for repl.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: sveltePreprocess({
+		scss: {
+			includePaths: ['src', 'node_modules'],
+		},
+	}),
 };
 
 export default config;
